b0d7ae72018f799781df084e29eef789514085f3
Andrei Pavlov <andrio.pavlov@gmail.com>
2022-06-24 23:58:04 +0300

    Added menu with choice of the form of writing expression.

--- calculator/main.py
+++ calculator/main.py
@@ -1,19 +1,35 @@
 import logging
+from sys import stdout
 
 from calculator import calculate
 
 
 logger = logging.getLogger('calculator')
-logger.addHandler(logging.StreamHandler())
+logger.addHandler(logging.StreamHandler(stdout))
+
 
 if __name__ == "__main__":
-    print('Enter expression in postfix form. Enter the exit to terminate the program.')
     while True:
-        expr = input()
-        if expr=='exit':
-            break
-        try:
-            result = calculate(expr, is_postfix=True)
-            print(result)
-        except (ArithmeticError, ValueError) as ex:
-            logger.error(ex)
+        print('Choose in which form you plan to enter expressions: infix or postfix. Enter "exit" to terminate the program.')
+        form = input()
+        if form not in ['infix', 'postfix', 'exit']:
+            print('Unknown command enter "infix", "postfix" or "exit"')
+            continue
+        if form == 'exit':
+            exit()
+
+        while True:
+            print('\nEnter expression in %s form. '
+                  'Enter "menu" to go to the selection of the form of the expression. '
+                  'Enter "exit" to terminate the program.' % form)
+            expr = input()
+            if expr == 'exit':
+                exit()
+            elif expr == 'menu':
+                break
+            try:
+                is_postfix = form=='postfix'
+                result = calculate(expr, is_postfix=is_postfix)
+                print(result)
+            except (ArithmeticError, ValueError) as ex:
+                logger.error(ex)

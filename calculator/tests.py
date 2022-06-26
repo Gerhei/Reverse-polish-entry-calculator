5219fc543fd8a4fe30d94962dd0d1cea75d31930
Andrei Pavlov <andrio.pavlov@gmail.com>
2022-06-26 23:31:36 +0300

    Added tests for expressions with variables.

--- calculator/tests.py
+++ calculator/tests.py
@@ -5,8 +5,6 @@ from calculator import *
 
 
 class CalculatorTestCase(unittest.TestCase):
-    allowed_error = 0.001
-
     def test_incorrect_float_number(self):
         incorrect_float_numbers = ['1.2.3', '1..2', '..4..', '.3.4', '.57.']
         for num in incorrect_float_numbers:
@@ -15,18 +13,16 @@ class CalculatorTestCase(unittest.TestCase):
 
     def test_correct_infix_expressions(self):
         corr_expr = {'1+2': 3, '2*(3+4)': 14, '-2+1': -1, '1.2*4': 4.8, '01.4-5': -3.6, '.43+5': 5.43,
-                     '-.67+1': 0.33, '2/4^2': 0.125, '(3+2)*(4-1)': 15, '-4': -4, '-(4+5)': -9, '4.+1': 5,
-                     '2*(-4+1)': -6.0}
+                     '-.5+1': 0.5, '2/4^2': 0.125, '(3+2)*(4-1)': 15, '-4': -4, '-(4+5)': -9, '4.+1': 5,
+                     '2*(-4+1)': -6.0, '2^(1+3)': 16}
         for expr, result in corr_expr.items():
-            is_equal = abs(result - calculate(expr, is_postfix=False)) <= self.allowed_error
-            self.assertTrue(is_equal)
+            self.assertEqual(result, calculate(expr, is_postfix=False))
 
     def test_correct_postfix_expression(self):
         corr_expr = {'2 2 +': 4, '2 3 + 5 6 + *': 55, '5 - 1 +': -4, '0.5 2 *': 1.0, '01.4 5 -': -3.6,
                      '3 3 / 2 *': 2, '4 - 1 + 2 *': -6, '4 5 + -': -9, '4 -': -4, '2 - 4 +': 2}
         for expr, result in corr_expr.items():
-            is_equal = abs(result - calculate(expr, is_postfix=True)) <= self.allowed_error
-            self.assertTrue(is_equal)
+            self.assertEqual(result, calculate(expr, is_postfix=True))
 
     def test_incorrect_infix_brackets(self):
         expr_with_brackets = ['((4+5)', ')-4*(7+8)', ')(', '(5-7)/(3*6', '(((1+2', '(5*6))']
@@ -69,6 +65,21 @@ class CalculatorTestCase(unittest.TestCase):
         for expr in expr_with_extra_operands:
             with self.assertRaises(ArithmeticError):
                 calculate(expr, is_postfix=True)
+
+    @skip
+    def test_infix_expression_with_variables(self):
+        expr_with_variables = {'2+a': '2+a', 'a+b': 'a+b', '2+4+a': '6+a', '2+a+b+3': '5+a+b'}
+        for expr, result in expr_with_variables.items():
+            self.assertEqual(result, calculate(expr, is_postfix=False))
+
+    def test_postfix_expression_with_variables(self):
+        expr_with_variables = {'4 a +': '4 a +', '5 4 + a *': '9.0 a *', '2 a + b + 3 +': '5.0 a + b +',
+                               '2 3 + a + b +': '5.0 a + b +', '2 a + 3 +': '5.0 a +', 'a 2 +': '2 a +',
+                               'a 2 + 3 + b +': '5.0 a + b +', 'a 2 + 3 +': '5.0 a +',
+                               'a b + 3 +': 'a b + 3 +', '2 a * 3 +': '2 a * 3 +', '2 a * 3 *': '6.0 a *',
+                               '2 a b * + 3 +': 'a b * 5 +'}
+        for expr, result in expr_with_variables.items():
+            self.assertEqual(result, calculate(expr, is_postfix=True))
 
 
 if __name__ == '__main__':
